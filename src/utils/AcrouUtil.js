f63a333a82457d06660d8a66461b7eded7128edb
NUSC <yangwenchao94@gmail.com>
2021-05-28 20:17:13 +0800

    Update AcrouUtil.js

--- src/utils/AcrouUtil.js
+++ src/utils/AcrouUtil.js
@@ -151,12 +151,12 @@ export function utc2beijing(utc_datetime) {
 }
 
 export function formatFileSize(bytes) {
-  if (bytes >= 1000000000) {
-    bytes = (bytes / 1000000000).toFixed(2) + " GB";
-  } else if (bytes >= 1000000) {
-    bytes = (bytes / 1000000).toFixed(2) + " MB";
-  } else if (bytes >= 1000) {
-    bytes = (bytes / 1000).toFixed(2) + " KB";
+  if (bytes >= 1024000000) {
+    bytes = (bytes / 1024000000).toFixed(2) + " GB";
+  } else if (bytes >= 1024000) {
+    bytes = (bytes / 1024000).toFixed(2) + " MB";
+  } else if (bytes >= 1024) {
+    bytes = (bytes / 1024).toFixed(2) + " KB";
   } else if (bytes > 1) {
     bytes = bytes + " bytes";
   } else if (bytes == 1) {
